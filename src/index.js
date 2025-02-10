c3bf509bb496a98298c8a9b280f14530ef03f603
Anthony Abdalla <anthony@Anthony>
2025-02-09 22:45:26 -0500

    Created RESTful API for users

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 const app = express();
 const port = 3000;
 
@@ -8,9 +9,60 @@ app.use(express.json());
 // **************************************************************
 // Put your implementation here
 // If necessary to add imports, please do so in the section above
+const users=[];
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
+});
+
+app.post('/users', (req, res)=>{
+    if (!req.body.name || !req.body.email){
+        return res.status(400).send('Name or Email is missing');
+    }
+
+    const user={
+        id: uuidv4(),
+        name: req.body.name,
+        email: req.body.email
+    };
+    users.push(user);
+    return res.status(201).send(user);
+})
+
+app.get('/users/:id', (req, res) => {
+    const user=users.find(u=>u.id===req.params.id);
+
+    if (!user){
+        return res.status(404).send('User does not exist')
+    }
+
+    return res.status(200).send(user);
+});
+
+app.put('/users/:id', (req, res)=>{
+    const user=users.find(u=>u.id===req.params.id);
+    if (!user){
+        return res.status(404).send('User does not exist')
+    }
+
+    if (!req.body.name || !req.body.email){
+        return res.status(400).send('Name or Email is missing');
+    }
+
+    user.name=req.body.name;
+    user.email=req.body.email;
+    return res.status(200).send(user);
+});
+
+app.delete('/users/:id', (req, res)=>{
+    const user=users.find(u=>u.id===req.params.id);
+    if (!user){
+        return res.status(404).send('User does not exist')
+    }
+
+    const index=users.indexOf(user);
+    users.splice(index, 1);
+    return res.status(204).send();
 });
 
 // Do not touch the code below this comment
